3a04ccaf0b1b207f038ee7f7add79ef8c8255b44
Gabau <au.gabriel@yahoo.com.sg>
2021-12-13 11:55:42 +0800

    Add build in upload step

--- coverage.sh
+++ coverage.sh
@@ -2,14 +2,16 @@
 
 # use to generate coverage report
 
-# mkdir -p build
-# cd build
-# cmake -G Ninja build ../
-# ninja
-# ninja cresser_coverage
+mkdir -p build
+cd build
+cmake -G Ninja build ../
+ninja
+ninja cresser_coverage
 # xdg-open cresser_coverage/index.html
 
 # Used to generate codecov report
+
+
 
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
