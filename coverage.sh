0684e955688168309dd3b1451e554bd59e7001a7
Gabau <au.gabriel@yahoo.com.sg>
2021-12-13 11:49:20 +0800

    Change to coverage.info

--- coverage.sh
+++ coverage.sh
@@ -2,10 +2,17 @@
 
 # use to generate coverage report
 
-mkdir -p build
-cd build
-cmake -G Ninja build ../
-ninja
-ninja cresser_coverage
-xdg-open cresser_coverage/index.html
+# mkdir -p build
+# cd build
+# cmake -G Ninja build ../
+# ninja
+# ninja cresser_coverage
+# xdg-open cresser_coverage/index.html
 
+# Used to generate codecov report
+
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+lcov --list coverage.info
+# Upload to codecov
+bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
